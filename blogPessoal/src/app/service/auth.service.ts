13d3c50809aa7c83427b60afb72a4867e848fa19
thaisbaraoh <thais-barao@hotmail.com>
2021-06-08 20:49:53 -0300

    post e get de potagens

--- blogPessoal/src/app/service/auth.service.ts
+++ blogPessoal/src/app/service/auth.service.ts
@@ -1,14 +1,15 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
-import { environment } from 'src/environments/environment.prod'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
 
   constructor(
     private http: HttpClient
@@ -16,19 +17,21 @@ export class AuthService {
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{
     return this.http.post<UserLogin>(`${environment.server}/usuarios/logar`, userLogin)
-
   }
+
   cadastrar(user: User): Observable<User>{
-    return this.http.post<User>(`${environment.server}/usuarios/cadastrar`, user)
-
+return this.http.post<User>(`${environment.server}/usuarios/cadastrar`,  user)
   }
 
-  logado (){
-    let ok : Boolean = false
-    
-    if (environment.token != ''){
+  getByIdUser(id: number): Observable<User>{
+return this.http.get<User>(`${environment.server}/usuarios/${id}`)
+  }
+
+  logado(){
+      let ok = false
+    if(environment.token !=  ''){
       ok = true
     }
-    return ok
+      return ok
   }
 }
